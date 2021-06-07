8bc6c537099da06064a243603f19b83f5bd01d65
星冉 <troy@zygd.site>
2021-06-07 16:35:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,25 @@
+#!/bin/sh
+clear
+# [banner]
+
+echo -e "\n\033[1;34m ______  ______     ___  \033[0m"
+echo -e "\033[1;32m |  _  \ | ___ \   |_  | \033[0m"
+echo -e "\033[1;36m | | | | | ___ \     | | \033[0m"
+echo -e "\033[1;31m | |/ /  | |_/ / /\__/ / \033[0m"
+echo -e "\033[1;35m |___/   \____/  \____/ \n\033[0m"
+echo -e "\033[1;36m                 Wgpsec \n\033[0m"
+
+# [help]
+echo -e "\033[1;32m [help] \033[0m"
+echo -e "\033[1;36m https://github.com/wgpsec/DBJ \n\033[0m"
+echo -e "\033[1;36m Update DBJ ✨\n\033[0m"
+/usr/bin/git -C /DBJ/ pull
+echo -e "\033[1;36m Update DBJ-PY-Tools ✨\n\033[0m"
+/usr/bin/pip3 install -r /DBJ/requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
+echo -e "\033[1;36m Start DBJ ✨\n\033[0m"
+
+# [main]
+
 export FLASK_APP=flaskr                 #设置运行环境
 export FLASK_ENV=development    #开启debug模式，有错误可以调试一下
 export LC_ALL=C.UTF-8
